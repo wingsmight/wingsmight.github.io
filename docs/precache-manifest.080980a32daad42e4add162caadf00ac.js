c957375f0ed92aa7600c0d6619b0d41510381d2d
Wings' might <wingsmight@gmail.com>
2021-06-19 00:49:35 +0700

    deploy throw docs

---
+++ docs/precache-manifest.080980a32daad42e4add162caadf00ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4138c6332ea1bc0171d2505f2e873b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4fa178da0e6e2f32de7",
+    "url": "/static/css/main.8673bdf4.chunk.css"
+  },
+  {
+    "revision": "88f09e95bb994015723e",
+    "url": "/static/js/2.8af92c41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8af92c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fa178da0e6e2f32de7",
+    "url": "/static/js/main.c72eb24b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "657a3740da8b2c23d4d8f61a955683c7",
+    "url": "/static/media/wm.657a3740.svg"
+  }
+]);
